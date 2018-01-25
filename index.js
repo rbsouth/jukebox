cb9507dab2354ef987225b3bbedfacb6738afadf
Reid South <rbsouth@gmail.com>
2018-01-24 17:44:20 -0700

    time display fixes #9, fixes #10

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ class Jukebox {
 		this.songs = ['songs/bye.mp3', 'songs/21-guns.mp3', 'songs/6foot7foot.mp3'];
 		this.currentSongIndex = 0;
 	}
+	
 	play(){
 		this.audio.play();
 	}
@@ -20,7 +21,23 @@ class Jukebox {
 		this.audio.setAttribute("src", this.songs[this.currentSongIndex]);
 		this.audio.play();
 	}
+	trackTime(time_display){
+		var current_time_minutes = Math.floor(this.audio.currentTime / 60);
+		var current_time_seconds = Math.floor(this.audio.currentTime - current_time_minutes * 60);
+		var duration_minutes = Math.floor(this.audio.duration / 60);
+		var duration_seconds = Math.floor(this.audio.duration - duration_minutes * 60);
+		if (current_time_minutes < 10) {
+			current_time_minutes = ' ' + current_time_minutes;
+		}
+		if (current_time_seconds < 10) {
+			current_time_seconds = '0' + current_time_seconds;
+		}
+		time_display.innerHTML = current_time_minutes + ':' + current_time_seconds + ' / ' + duration_minutes + ':' + duration_seconds + '';
+		
+	}
 }
+
+
 
 var song = document.getElementById('song');
 var jukebox = new Jukebox(song);
@@ -46,4 +63,10 @@ var skip_song = document.getElementById('skip-button');
 
 skip_song.addEventListener('click', function(){
 	jukebox.skip();
+})
+
+//listen for timeupdate 
+var realtime = document.getElementById('realtime')
+song.addEventListener('timeupdate', function(){
+		jukebox.trackTime(realtime)
 })
